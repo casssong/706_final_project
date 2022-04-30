c86afb3f78b094723a7dd3254b981940b4deccdd
Catherine <catherinejsong96@gmail.com>
2022-04-29 23:47:59 -0400

    testing world map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from vega_datasets import data
 
 alt.data_transformers.disable_max_rows()
 
@@ -14,11 +15,11 @@ covid_df = covid_df.rename(columns = {'location':'Country'})
 covid_df = covid_df.merge(country_df[['Country','country-code']],how='left', on=['Country'])
 
 #world data
-data_world = data[data['Country']=='World']
+data_world = covid_df[covid_df['Country']=='World']
 
 #country data
 country_drop =  ['Africa', 'Asia', 'Europe', 'European Union', 'High income', 'International', 'Low income', 'Lower middle income', 'North America', 'Oceania', 'South America', 'Upper middle income', 'World']
-data_country = data.copy()
+data_country = covid_df.copy()
 data_country = data_country[~pd.DataFrame(data_country.Country.tolist()).isin(country_drop).any(1).values]
 """
 drop population < 500,000 (44)
@@ -43,6 +44,59 @@ death, case, vaccination, population
 3 maps, linked together, one country
 x - month
 """
+# Map - total cases, total deaths
+df_map = data_country.groupby(['Country', 'total_cases', 'total_deaths','population','year']).sum().reset_index()
+#df2['Rate'] = df2['Death']/df2['Pop'] * 1000000
+source = alt.topo_feature(data.world_110m.url, 'countries')
+year = 2021 # only visualize for 2021
+data_country = data_country[data_country['year']==year]
+width = 600
+height  = 300
+project = 'equirectangular'
+
+
+# a gray map using as the visualization background
+background = alt.Chart(source
+                       ).mark_geoshape(
+    fill='#aaa',
+    stroke='white'
+).properties(
+    width=width,
+    height=height
+).project(project)
+
+
+selector = alt.selection_single(
+    fields=['id']
+)
+
+
+chart_base = alt.Chart(source
+                       ).properties(
+    width=width,
+    height=height
+).project(project
+          ).add_selection(selector
+                          ).transform_lookup(
+    lookup="id",
+    from_=alt.LookupData(df_map, "country-code", ["total_cases", 'Country', 'total_deaths','population' ,'year']),
+)
+
+# fix the color schema so that it will not change upon user selection
+case_scale = alt.Scale(domain=[df_map['total_cases'].min(), df_map['total_cases'].max()])
+case_color = alt.Color(field="total_cases", type="quantitative", scale=case_scale)
+chart_case = chart_base.mark_geoshape().encode(
+    ######################
+    # P3.1 map visualization showing the mortality rate
+    color=case_color,
+    ######################
+    # P3.3 tooltip
+    tooltip= ['total_cases:Q', 'Country:N']
+).transform_filter(
+    selector
+).properties(
+    title=f'Total Cases Worldwide {year}'
+)
 
 #selector month:
 
