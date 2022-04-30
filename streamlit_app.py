ddc54b75900b3306a813aca671c60e73913410ab
jiazi1 <jiazi_tian@hms.harvard.edu>
2022-04-29 22:14:59 -0400

    data load

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,27 @@ import streamlit as st
 @st.cache
 
 #load data
-#add country-codes
 covid_df = pd.read_csv('processed_data.csv')
+#add country-codes
 country_df = pd.read_csv('https://raw.githubusercontent.com/hms-dbmi/bmi706-2022/main/cancer_data/country_codes.csv', dtype = {'conuntry-code': str})
-covid_df=covid_df.rename(columns = {'location':'Country'})
+covid_df = covid_df.rename(columns = {'location':'Country'})
 covid_df = covid_df.merge(country_df[['Country','country-code']],how='left', on=['Country'])
+
+#world data
+data_world = data[data['Country']=='World']
+
+#country data
+country_drop =  ['Africa', 'Asia', 'Europe', 'European Union', 'High income', 'International', 'Low income', 'Lower middle income', 'North America', 'Oceania', 'South America', 'Upper middle income', 'World']
+data_country = data.copy()
+data_country = data_country[~pd.DataFrame(data_country.Country.tolist()).isin(country_drop).any(1).values]
 
 st.write("## COVID-19 cases from 2021 to 2022")
 
-#bar plot global: 
+#line plot global: 
 
-
-#pie chart multiple countries:
-
+#selector month:
 
 #map specific countries:
 
-#line plot:
-
+#
 #bar plot:
