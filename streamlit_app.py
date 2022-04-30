54790e3ecdd47d40991d57749bfab97b5eab63f0
Catherine <catherinejsong96@gmail.com>
2022-04-30 11:58:34 -0400

    correct chart vac error

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,7 +121,7 @@ chart_pop = chart_base.mark_geoshape().encode(
 )
 
 vac_scale = alt.Scale(domain=[df_map['people_vaccinated'].min(), df_map['people_vaccinated'].max()])
-vac_color = alt.Color(field="vaccination", type="quantitative", scale=vac_scale, legend=alt.Legend(title="Total Vaccination"))
+vac_color = alt.Color(field="people_vaccinated", type="quantitative", scale=vac_scale, legend=alt.Legend(title="Total Vaccination"))
 chart_vac = chart_base.mark_geoshape().encode(
     ######################
     # P3.2 map visualization showing the mortality rate
@@ -138,8 +138,6 @@ chart_vac = chart_base.mark_geoshape().encode(
 chart_map = alt.vconcat(background + chart_pop, background + chart_case, background + chart_death, background + chart_vac).resolve_scale(
     color='independent')
 
-
-# st.altair_chart(chart_map, use_container_width=True)
 st.altair_chart(chart_map)
 
 
