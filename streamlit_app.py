53c9244a979a8691ace9fc46f7db8a5e648c75f3
Catherine <catherinejsong96@gmail.com>
2022-04-30 13:37:39 -0400

    changing legend

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,7 @@ chart_line = alt.Chart(df_line).mark_line().encode(
     x = alt.X('month:N', title='Month'),
     y = alt.Y('value', title="Total Number of People"),
     color = alt.condition(data_selection, "Global_data", alt.value('lightgray'), legend=alt.Legend(title="Data Type")),
-
     tooltip=[alt.Tooltip("month", title="Month"), alt.Tooltip("value:Q", title='Number of People')]
-
 ).add_selection(
     data_selection
 ).properties(
